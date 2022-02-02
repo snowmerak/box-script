0e0a05668a0c1a6034038cd8b4b87e5797903483
snowmerak <snowmerak@outlook.com>
2022-02-03 01:24:30 +0900

    Update statement.go

--- grammar/statement.go
+++ grammar/statement.go
@@ -1,7 +1,7 @@
 package grammar
 
 type Statement struct {
-	Using     *Using     `"using" @@`
+	Using     *string    `  "using" @String`
 	Let       *Let       `| "let" @@`
 	Enum      *Enum      `| "enum" @@`
 	Message   *Message   `| "message" @@`
@@ -15,6 +15,20 @@ type Type struct {
 	Name []string `@Ident ("." @Ident)*`
 }
 
-type Using struct {
-	Name *string `"using" @String`
-}
+const (
+	Int8    = "int8"
+	Int16   = "int16"
+	Int32   = "int32"
+	Int64   = "int64"
+	Uint8   = "uint8"
+	Uint16  = "uint16"
+	Uint32  = "uint32"
+	Uint64  = "uint64"
+	Byte    = Uint8
+	Rune    = Int32
+	Float32 = "float32"
+	Float64 = "float64"
+	String  = "string"
+	Char    = "char"
+	Bool    = "bool"
+)
